8871f5d945887b0b3332655c557d22ceec18c708
Emil Muller <emil@alumis.no>
2020-02-04 21:27:50 +0100

    no message

--- templates/simple/src/index.tsx
+++
@@ -1,3 +0,0 @@
-import styles from "./index.scss";
-
-document.body.appendChild(<p className={styles.green}>Hello, World!</p>);
--- templates/simple/webpack.config.dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = require("./webpack.config.base")({
-  path: "dist",
-  isDevelopment: true,
-  constants: {
-  }
-});
